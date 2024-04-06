5c6b24fb48ff6d553b6b50c43b681f4e657ce97e
Vladimir1t <vova.vexov@mail.ru>
2024-04-06 17:58:30 +0300

    add prototip of a recursive infix calculator

---
+++ src/recursive_descent.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+const char* str = NULL;
+const int SYNTAX_ERROR = 0xDEAD;
+
+int get_G (const char* String);
+int get_E ();
+int get_T ();
+int get_P ();
+int get_N ();
+int syntax_error ();
+
+int main ()
+{
+    char String[] = "(2+(2*5))*3$";
+
+    int value = get_G (String);
+
+    printf ("value = %d\n", value);
+
+    return 0;
+}
+
+int get_G (const char* String)
+{
+    printf ("G\n");
+    str = String;
+    int val = get_E ();
+
+    if (*str == '$')
+        str++;
+    else
+        syntax_error ();
+
+    return val;
+}
+
+int get_E ()
+{
+    printf ("E\n");
+    int val = get_T ();
+    while (*str == '+' || *str == '-')
+    {
+        const char* op = str;
+        str++;
+        int val_2 = get_T ();
+        if (*op == '+')
+            val += val_2;
+        else if (*op == '-')
+            val -= val_2;
+    }
+    return val;
+}
+
+int get_T ()
+{
+    printf ("T\n");
+    int val = get_P ();
+    while (*str == '*' || *str == '/')
+    {
+        const char* op = str;
+        str++;
+        int val_2 = get_P ();
+        if (*op == '*')
+            val *= val_2;
+        else if (*op == '/')
+            val /= val_2;
+    }
+    return val;
+}
+
+int get_P ()
+{
+    printf ("P\n");
+    int val = 0;
+    if (*str == '(')
+    {
+        str++;
+        val = get_E ();
+        if (*str == ')')
+        {
+            str++;
+            return val;
+        }
+    }
+    else
+        val = get_N ();
+
+    return val;
+}
+
+int get_N ()
+{
+    printf ("N\n");
+    int val = 0;
+    const char* old_str = str;
+    while ('0'<= *str && *str <= '9')
+    {
+        val = val * 10 + (*str - '0');
+        str++;
+    }
+
+    if (old_str == str)
+    {
+        syntax_error ();
+    }
+
+    return val;
+}
+
+int syntax_error ()
+{
+    printf ("syntax error\n");
+    getchar ();
+    return SYNTAX_ERROR;
+}
