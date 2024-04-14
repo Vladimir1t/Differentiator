bfc4586e36a848a4277f39a66816fcf192021ea6
Vladimir1t <vova.vexov@mail.ru>
2024-04-14 20:39:28 +0300

    change error_file

--- library/library_of_defines.h
+++ library/library_of_defines.h
@@ -48,18 +48,24 @@
         return (fabs double - double) <= EPSILON);       \
     }
 
+#define SUBTREE_DTOR(node)      \
+    do                          \
+    {                           \
+        free(node);             \
+        node = NULL;            \
+    }                           \
+    while (0)
+
 #define ADD_DIFF(node)                                                                     \
     {                                                                                      \
         unsigned char add = '+';                                                           \
         return create_node (T_OP, &add, diff (node->left, var), diff (node->right, var));  \
-        break;                                                                             \
     }
 
 #define SUB_DIFF(node)                                                                     \
     {                                                                                      \
         unsigned char sub = '-';                                                           \
         return create_node (T_OP, &sub, diff (node->left, var), diff (node->right, var));  \
-        break;                                                                             \
     }
 
 #define MUL_DIFF(node)                                                       \
@@ -74,16 +80,13 @@
                                                                              \
         return create_node (T_OP, &add, create_node (T_OP, &mul, du, v),     \
                                         create_node (T_OP, &mul, u, dv));    \
-        break;                                                               \
     }
 
 #define DIV_DIFF(node)                                                                      \
     {                                                                                       \
         unsigned char mul = '*', sub = '-', div = '/';                                      \
-                                                                                            \
         struct Node* du = diff (node->left, var);                                           \
         struct Node* u  = copy_subtree (node->left);                                        \
-                                                                                            \
         struct Node* dv = diff (node->right, var);                                          \
         struct Node* v  = copy_subtree (node->right);                                       \
                                                                                             \
@@ -91,9 +94,45 @@
                                                           create_node (T_OP, &mul, u, dv)); \
         struct Node* denominator = create_node (T_OP, &mul, copy_subtree (u),               \
                                                             copy_subtree (u));              \
-                                                                                            \
         return create_node (T_OP, &div, nominator, denominator);                            \
-        break;                                                                              \
+    }
+
+#define LN_DIFF(node)                                                                                   \
+    {                                                                                                   \
+        unsigned char mul = '*', div = '/';                                                             \
+        double value = 1;                                                                               \
+        struct Node* val = copy_subtree (node->left);                                                   \
+        struct Node* dln = create_node (T_OP, &div,  create_node (T_NUM, &value, NULL, NULL), val);     \
+        return create_node (T_OP, &mul, dln, diff (node->left, var));                                   \
+    }
+
+#define SIN_DIFF(node)                                                                                  \
+    {                                                                                                   \
+        unsigned char mul = '*';                                                                        \
+        char* cos = "cos";                                                                              \
+        struct Node* val = copy_subtree (node->left);                                                   \
+        struct Node* dsin = create_node (T_OP_LONG, cos, val, NULL);                                    \
+        return create_node (T_OP, &mul, dsin, diff (node->left, var));                                  \
+    }
+
+#define COS_DIFF(node)                                                                                  \
+    {                                                                                                   \
+        printf ("diff cos\n");                                                                          \
+        unsigned char mul = '*', sub = '-';                                                             \
+        double value = 0;                                                                               \
+        char* sin = "sin";                                                                              \
+        struct Node* val_1 = copy_subtree (node->left);                                                 \
+        struct Node* val_2 = create_node (T_OP_LONG, sin, val_1, NULL);                                 \
+        struct Node* dcos = create_node (T_OP, &mul, val_2, diff (node->left, var));                    \
+        return create_node (T_OP, &sub, create_node (T_NUM, &value, NULL, NULL), dcos);                 \
+    }
+
+#define EXP_DIFF(node)                                                                                  \
+    {                                                                                                   \
+        unsigned char mul = '*';                                                                        \
+        Node* val = copy_subtree (node);                                                                \
+        Node* res = create_node (T_OP, &mul, val, diff (node->left, var));                              \
+        return res;                                                                                     \
     }
 
 #endif // DSL_H_INCLUDED
