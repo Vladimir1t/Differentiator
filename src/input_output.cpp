4fd6507450f8e73d3868153108d62b7c6ab92bae
Vladimir1t <vova.vexov@mail.ru>
2024-04-28 17:54:59 +0300

    Update input_output.cpp

--- src/input_output.cpp
+++ src/input_output.cpp
@@ -2,15 +2,17 @@
 
 static FILE* file_error = fopen ("log\\input_output_error.txt", "w");
 
+void skip_symbols (char** ptr);
+
 static struct Node* get_g (const char* text_data);
 
 static struct Node* get_e (char** ptr);
 
 static struct Node* get_t (char** ptr);
 
-static struct Node* get_d (char** ptr);
-
 static struct Node* get_p (char** ptr);
+
+struct Node* get_pow (char** ptr);
 
 static struct Node* get_f (char** ptr);
 
@@ -30,8 +32,6 @@ int get_database (struct Node** root, char* file_input)       // get data of tre
 
     FOPEN (file_p, file_input, "rb");
     size_t file_size = file_size_measure (file_p);            // measures the size of a text
-    //printf ("size of file: %d\n", file_size);
-
     char* text_data = NULL;
     CALLOC (text_data, char, file_size + 1);
 
@@ -42,28 +42,34 @@ int get_database (struct Node** root, char* file_input)       // get data of tre
         return ERROR;
     }
     fclose (file_p);
-    text_data[file_size] = '$';
+    //text_data[file_size] = '$';
 
     *root = get_g (text_data);
 
     if (root == NULL)
         return ERROR;
-    //construct_data_nodes (*root, text_data, file_size);
 
     return SUCCESS;
 }
 
+void skip_symbols (char** ptr)
+{
+    while (**ptr == '\n' || **ptr == ' ')
+        *ptr += 1;
+}
 
 struct Node* get_g (const char* text_data)
 {
-    //printf ("G\n");
+    printf ("G\n");
     char* ptr = (char*) text_data;
     struct Node* value = get_e (&ptr);
 
     while (*ptr == '\n' || *ptr == '\r')
         ptr += 1;
 
-    if (*ptr == '$')
+    skip_symbols (&ptr);
+
+    if (*ptr == '\0')
         ptr += 1;
 
     else
@@ -77,12 +83,14 @@ struct Node* get_g (const char* text_data)
 
 struct Node* get_e (char** ptr)    //  + or -
 {
-    //printf ("E\n");
+    printf ("E\n");
     struct Node* value_1 = get_t (ptr);
+    skip_symbols (ptr);
     while (**ptr == '+' || **ptr == '-')
     {
         const char op = **ptr;
         *ptr += 1;
+        skip_symbols (ptr);
         struct Node* value_2 = get_t (ptr);
 
         if (op == '+')
@@ -103,12 +111,14 @@ struct Node* get_e (char** ptr)    //  + or -
 
 struct Node* get_t (char** ptr)   //   * or /
 {
-    //printf ("T\n");
+    printf ("T\n");
     struct Node* value_1 = get_p (ptr);
+    skip_symbols (ptr);
     while (**ptr == '*' || **ptr == '/')
     {
         const char op = **ptr;
         *ptr += 1;
+        skip_symbols (ptr);
         struct Node* value_2 = get_p (ptr);
 
         if (op == '*')
@@ -129,31 +139,31 @@ struct Node* get_t (char** ptr)   //   * or /
 
 struct Node* get_p (char** ptr)   //  (, )  and determine functions
 {
-    //printf ("P\n");
+    printf ("P\n");
     struct Node* value = NULL;
 
     if (**ptr == '(')
     {
         *ptr += 1;
+        skip_symbols (ptr);
         value = get_e (ptr);
 
         if (**ptr == ')')
         {
             *ptr += 1;
+            skip_symbols (ptr);
             if (**ptr == '^')
             {
-                *ptr += 1;
                 unsigned char pow = '^';
-                struct Node* value_2 = get_p (ptr);
+                *ptr += 1;
+                skip_symbols (ptr);
+                struct Node* value_2 = get_pow (ptr);
                 value = create_node (T_OP, &pow, value, value_2);
             }
             return value;
         }
     }
-    else if (**ptr == 'l' && *(*ptr + 1) == 'n')
-        value = get_d (ptr);
-
-    else if (isalpha (**ptr) && isalpha (*(*ptr + 1))  && isalpha (*(*ptr + 2)))
+    else if (isalpha (**ptr))
         value = get_f (ptr);
 
     else
@@ -162,82 +172,73 @@ struct Node* get_p (char** ptr)   //  (, )  and determine functions
     return value;
 }
 
-struct Node* get_f (char** ptr)    //  cos, sin, exp
+struct Node* get_pow (char** ptr)
 {
-    //printf ("F\n");
-    struct Node* value_1 = NULL;
-    char op = **ptr;
-    *ptr += 3;
+    printf ("Pow\n");
+    struct Node* value = NULL;
 
     if (**ptr == '(')
     {
         *ptr += 1;
-        if (op =='c')
+        skip_symbols (ptr);
+        value = get_e (ptr);
+        if (**ptr == ')')
         {
-            char* cos = "cos";
-            value_1 = get_e (ptr);
-            value_1 = create_node (T_OP_LONG, cos, value_1, NULL);
-        }
-        else if (op == 's')
-        {
-            char* sin = "sin";
-            value_1 = get_e (ptr);
-            value_1 = create_node (T_OP_LONG, sin, value_1, NULL);
-        }
-        else if (op == 'e')
-        {
-            char* exp = "exp";
-            value_1 = get_e (ptr);
-            value_1 = create_node (T_OP_LONG, exp, value_1, NULL);
+            *ptr += 1;
+            return value;
         }
         else
-        {
-            syntax_error ();
-            return NULL;
-        }
-        if (**ptr == ')')
-            *ptr += 1;
+            return syntax_error ();
     }
-    return value_1;
+    else
+        return syntax_error ();
 }
 
-struct Node* get_d (char** ptr)   //   ln
+
+struct Node* get_f (char** ptr)
 {
-    //printf ("D\n");
-    *ptr += 2;
-    struct Node* value_1 = NULL;
-
-    if (**ptr == '(')
+    printf ("F\n");
+    struct Node* value = NULL;
+    char str[15] = {0};
+    int i = 0;
+    while (isalpha (**ptr))
     {
+        str[i++] = **ptr;
         *ptr += 1;
-        char* ln = "ln";
-        value_1 = get_e (ptr);
-        value_1 = create_node (T_OP_LONG, ln, value_1, NULL);
-        if (**ptr == ')')
-        {
-            *ptr += 1;
-        }
     }
+    skip_symbols (ptr);
+    if (**ptr == '(')             // function
+    {
+        for (int j = 0; j < OP_NUM; j++)
+           if (!strcmp (str, array_op[j].name))
+           {
+                value = get_p (ptr);
+                value = create_node (T_OP_LONG, array_op[j].name, value, NULL);
+                return value;
+           }
+        return syntax_error ();
+    }
+    else if (isdigit (**ptr))     // fdjk1
+        return syntax_error ();
 
-    return value_1;
+    else if (strlen (str) == 1)   // variable
+    {
+        value = (struct Node*) calloc (1, sizeof (struct Node));
+        value->type = T_VAR;
+        value->data.var = *(unsigned char*) str;
+    }
 }
 
 struct Node* get_n (char** ptr)   //determine numbers and variables
 {
-    //printf ("N\n");
+    printf ("N\n");
     const char* old_ptr = *ptr;
 
-    struct Node* val = (struct Node*) calloc (1, sizeof (struct Node));
+    struct Node* value = (struct Node*) calloc (1, sizeof (struct Node));
 
-    if (isalpha (**ptr))
+    if (isdigit (**ptr))
     {
-        val->type = T_VAR;
-        val->data.var = **ptr;
-        *ptr += 1;
-    }
-    else
-    {
-        val->type = T_NUM;
+        value->type = T_NUM;
 
         char sign = '+';
         if (**ptr == '-')
@@ -246,20 +247,19 @@ struct Node* get_n (char** ptr)   //determine numbers and variables
         while ('0'<= **ptr && **ptr <= '9')
         {
             if (sign == '+')
-                val->data.value = val->data.value * 10 + (**ptr - '0');
+                value->data.value = value->data.value * 10 + (**ptr - '0');
             else
-                val->data.value = val->data.value * 10 - (**ptr - '0');
+                value->data.value = value->data.value * 10 - (**ptr - '0');
             *ptr += 1;
         }
     }
+    else
+        syntax_error ();
 
     if (*ptr == old_ptr)
-    {
-        syntax_error ();
-        return NULL;
-    }
+        return syntax_error ();
 
-    return val;
+    return value;
 }
 
 struct Node* syntax_error ()
